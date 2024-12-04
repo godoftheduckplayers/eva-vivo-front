14ac4f8ed858448a0676f635dc4e43e66c4c6b27
Leandro Marques <lmarqueo@emeal.nttdata.com>
2024-12-04 11:08:48 -0300

    Update app component with Angular Material and remove template
    Added Angular Material modules to app.component.ts for improved UI component functionality. Removed placeholder content from app.component.html, replacing it with a minimal structure for a cleaner setup. Deleted and re-added favicon icon and added a new image to the public directory.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NgOptimizedImage, MatToolbarModule, MatButtonModule, MatIconModule, MatFormFieldModule,
+    MatInputModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'eva-vivo-front';
+  protected readonly parent = parent;
 }
